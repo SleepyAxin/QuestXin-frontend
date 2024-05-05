addf1f58665cbf0bca34e1139db8c8b029b6ccdc
SleepyAxin <sleepyaxin@163.com>
2024-05-05 22:05:18 +0800

    000

---
+++ js/about.01926fd0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkquestxin"]=self["webpackChunkquestxin"]||[]).push([[594],{5609:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var e=t(6768);const s={class:"about"},a=(0,e.Lk)("h1",null,"This is an about page",-1),r=[a];function c(n,u){return(0,e.uX)(),(0,e.CE)("div",s,r)}var i=t(1241);const o={},f=(0,i.A)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.01926fd0.js.map
